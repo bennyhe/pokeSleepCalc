7540ac0036211c98271308742e5bde562dbf735e
bennyhe <hesiyuan009@gmail.com>
2024-02-18 18:49:09 +0800

    feat: 优化性能

--- src/pages/FoodRec.vue
+++ src/pages/FoodRec.vue
@@ -1,28 +1,29 @@
 <script setup>
 import CptPoke from '../components/CptPoke/ItemIndex.vue'
 import CptFoodmenu from '../components/CptFoodmenu/MenuItem.vue'
-import {
-  formatTime,
-  findMenuWithFood
-} from '../utils/index.js'
+import { formatTime, findMenuWithFood } from '../utils/index.js'
 import { pokedex } from '../config/pokedex.js'
 import { foodRecommend } from '../config/foodRecommend.js'
 import { POKE_TYPES, FOOD_TYPES } from '../config/valKey.js'
 
+const nFoodRecommend = { ...foodRecommend }
+nFoodRecommend.list.forEach(pokeItem => {
+  pokeItem.menuList = findMenuWithFood(pokedex[pokeItem.evoLine[0]].food.type)
+})
 </script>
 <template>
   <h2>食材宝可梦推荐</h2>
   <div class="page-inner mod-tips">
-    <p>创建时间：{{ formatTime(foodRecommend.creatTime) }}</p>
-    <p>最后更新：{{ formatTime(foodRecommend.updateTime) }}</p>
-    <p>本文基于app v{{ foodRecommend.appVer }}</p>
+    <p>创建时间：{{ formatTime(nFoodRecommend.creatTime) }}</p>
+    <p>最后更新：{{ formatTime(nFoodRecommend.updateTime) }}</p>
+    <p>本文基于app v{{ nFoodRecommend.appVer }}</p>
     <p>可能因为游戏更新会导致有修改，本人对此不负任何责任。</p>
     <p>
       新人优先树果队，不要玩食材队，食材队收益较低，千算万算不如人家树果队稳定，千算万算不如人家料理暴击一顿。需要扩食材包，需要解锁高级食材，需要严选宝可梦，需要扩锅手，组队难度大，培养成本高，做饭不暴击失落感强，容易弃坑。
     </p>
   </div>
   <ul class="cpt-list food-list">
-    <li v-for="(pokeItem, pokeKey) in foodRecommend.list" v-bind:key="pokeKey">
+    <li v-for="(pokeItem, pokeKey) in nFoodRecommend.list" v-bind:key="pokeKey">
       <p class="mod-tips">
         {{ POKE_TYPES[pokedex[pokeItem.evoLine[0]].pokeType] }}型
       </p>
@@ -73,17 +74,11 @@ import { POKE_TYPES, FOOD_TYPES } from '../config/valKey.js'
       <p class="desc" v-html="pokeItem.desc"></p>
       <div class="cpt-foodmenu-list">
         <h3>
-          关联食谱<span class="extra"
-            >({{
-              findMenuWithFood(pokedex[pokeItem.evoLine[0]].food.type).length
-            }}个)</span
-          >
+          关联食谱<span class="extra">({{ pokeItem.menuList.length }}个)</span>
         </h3>
         <div class="cpt-foodmenu-scroll">
           <CptFoodmenu
-            v-for="menuItem in findMenuWithFood(
-              pokedex[pokeItem.evoLine[0]].food.type
-            )"
+            v-for="menuItem in pokeItem.menuList"
             v-bind:key="menuItem.id"
             :menuItem="menuItem"
           />
