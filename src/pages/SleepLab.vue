934e66c719cbaba1ef703421f1d3c9d4e858d359
bennyhe <hesiyuan009@gmail.com>
2024-03-11 21:46:16 +0800

    feat: 全地图期望

--- src/pages/SleepLab.vue
+++ src/pages/SleepLab.vue
@@ -3,11 +3,12 @@ import { ref } from 'vue'
 import { pokedex } from '../config/pokedex.js'
 import { gameMap } from '../config/game.js'
 import { getRandomHope, getLevelIndexByEnergy } from '../utils/sleep.js'
-import { getDecimalNumber,getNum } from '../utils/index.js'
+import { getDecimalNumber, getNum } from '../utils/index.js'
 import { SPO_DATA } from '../config/spo.js'
 import { SLEEP_STYLE } from '../config/sleepStyle.js'
-import { SLEEP_NAMES } from '../config/valKey.js'
+import { SLEEP_NAMES, SLEEP_TYPES } from '../config/valKey.js'
 import CptPoke from '../components/CptPoke/ItemIndex.vue'
+import CptSleepStyle from '../components/CptSleepStyle/SleepItem.vue'
 
 // const agm = [...gameMap]
 // for (let i = 5; i < agm.length; i++) {
@@ -49,38 +50,129 @@ import CptPoke from '../components/CptPoke/ItemIndex.vue'
 // })
 // console.log(arr)
 
+const pageData = ref({
+  curMap: 0,
+  mapSleepType: '1',
+  maxScore: 5000000
+})
+
 const getTimes = 4000
-const basePoint = 0
 const targetPokeId = 280
-const targetRes = []
 const testData = ref([])
-const startTime = new Date().getTime()
-const baseStarI = 595
-const splitPoint = 1000
-for (let i = baseStarI; i < baseStarI + 1; i++) {
-  const curAllScore = (basePoint + splitPoint * i)
-  const res = getRandomHope(
-    gameMap[4],
-    2,
-    curAllScore * 100,
-    getLevelIndexByEnergy(gameMap[4], curAllScore),
-    getTimes
-  )
-  console.log(i, new Date().getTime() - startTime)
-  const lastRes = {
-    basePoint: curAllScore,
-    allPoint: curAllScore * 100,
-    res
+// 0-10w 1000
+// 10w-50w 2000
+// 50w以上-400w 5000
+// 400w-500w 100000
+const handleClickGet = () => {
+  const basePoint = 0
+  const startTime = new Date().getTime()
+  const baseStarI = 0
+  const targetRes = []
+  let splitPoint = 1000
+  testData.value = []
+  for (let i = baseStarI; i < baseStarI + 2000; i++) {
+    const curAllScore = basePoint + splitPoint
+    const allPoint = curAllScore * 100
+    const res = getRandomHope(
+      gameMap[pageData.value.curMap],
+      +pageData.value.mapSleepType,
+      allPoint,
+      getLevelIndexByEnergy(gameMap[pageData.value.curMap], curAllScore),
+      getTimes
+    )
+    console.log(
+      i,
+      curAllScore,
+      allPoint,
+      `${(new Date().getTime() - startTime) / 1000}s`
+    )
+    const lastRes = {
+      basePoint: curAllScore,
+      allPoint,
+      res
+    }
+    targetRes.push(lastRes)
+    if (basePoint + splitPoint > 4000000) {
+      splitPoint += 100000
+    } else if (
+      basePoint + splitPoint > 500000 &&
+      basePoint + splitPoint <= 4000000
+    ) {
+      splitPoint += 5000
+    } else if (
+      basePoint + splitPoint > 100000 &&
+      basePoint + splitPoint <= 500000
+    ) {
+      splitPoint += 2000
+    } else {
+      splitPoint += 1000
+    }
+    if (curAllScore > +pageData.value.maxScore) {
+      break
+    }
   }
-  targetRes.push(lastRes)
+  testData.value = targetRes
+  console.log(targetRes)
 }
-testData.value = targetRes
-console.log(targetRes)
+
+const handleClickChangeMap = id => {
+  pageData.value.curMap = id
+}
 </script>
 
 <template>
+  <el-form label-width="90px">
+    <!-- S 当前岛屿 -->
+    <el-form-item label="当前岛屿">
+      <ul class="cpt-select-list">
+        <li
+          class="cpt-select-list__item"
+          v-for="(mapItem, mapIndex) in gameMap"
+          v-bind:key="mapItem.id"
+          :class="{ cur: pageData.curMap === mapIndex }"
+          @click="handleClickChangeMap(mapIndex)"
+        >
+          <div class="cpt-select-list__name">
+            {{ mapItem.name }}
+          </div>
+          <img
+            v-if="mapItem.pic"
+            class="cpt-select-list__bg"
+            v-lazy="`./img/ui/${mapItem.pic}.png`"
+            :alt="mapItem.name"
+          />
+        </li>
+      </ul>
+    </el-form-item>
+    <!-- E 当前岛屿 -->
+    <el-form-item label="睡眠类型">
+      <el-radio-group v-model="pageData.mapSleepType">
+        <el-radio
+          :label="cKey"
+          v-for="(cItem, cKey) in SLEEP_TYPES"
+          v-bind:key="cItem"
+          ><div class="i i-sleeptype" :class="`i i-sleeptype--${cKey}`">
+            {{ SLEEP_TYPES[cKey] }}
+          </div>
+        </el-radio>
+      </el-radio-group>
+    </el-form-item>
+    <el-form-item label="能量范围">
+      <el-input type="tel" v-model="pageData.maxScore">
+        <template #prefix>
+          <img class="icon" v-lazy="`./img/ui/energy.png`" /> 
+          0-</template
+      ></el-input>
+    </el-form-item>
+    <el-form-item>
+      <el-button @click="handleClickGet">计算结果</el-button>
+    </el-form-item>
+  </el-form>
+  <h2>
+    {{ gameMap[pageData.curMap].name }}-{{ SLEEP_TYPES[pageData.mapSleepType] }}
+  </h2>
   <div v-for="tdItem in testData" v-bind:key="tdItem.allPoint">
-    <h3>{{ getNum(tdItem.basePoint)}}-{{getNum(tdItem.allPoint) }}</h3>
+    <h3>{{ getNum(tdItem.basePoint) }}-{{ getNum(tdItem.allPoint) }}</h3>
     <div class="page-inner">
       <div
         class="cpt-avatar"
@@ -95,61 +187,65 @@ console.log(targetRes)
       </div>
     </div>
   </div>
-  <div>
-        <template v-for="gmItem in gameMap" v-bind:key="gmItem.id">
-          <h3>
-            {{ gmItem.name }}
-          </h3>
-          <div
-            class="poke-tb"
-            v-for="levelItem in gmItem.levelList"
-            v-bind:key="levelItem.energy"
+  <div class="hide">
+    <template v-for="gmItem in gameMap" v-bind:key="gmItem.id">
+      <h3>
+        {{ gmItem.name }}
+      </h3>
+      <div
+        class="poke-tb"
+        v-for="levelItem in gmItem.levelList"
+        v-bind:key="levelItem.energy"
+      >
+        <h4>
+          {{ levelItem.name }}
+          <span class="extra" v-if="levelItem.sleepStyles"
+            >{{ levelItem.sleepStyles.length }}只</span
           >
-            <h4>
-              {{ levelItem.name }}
-              <span class="extra" v-if="levelItem.sleepStyles"
-                >{{ levelItem.sleepStyles.length }}只</span
-              >
-            </h4>
-            <template v-for="sleepId in levelItem.sleepStyles">
-              <div
-                class="poke-tb__item"
-                v-if="SLEEP_STYLE[sleepId]"
-                v-bind:key="sleepId"
-              >
-                <CptPoke :pokeId="SLEEP_STYLE[sleepId].pokeId" />
-                <p>{{ SLEEP_STYLE[sleepId].id }}</p>
-                <p>{{ SLEEP_NAMES[SLEEP_STYLE[sleepId].sleepNameId] }}</p>
-                <p>{{ SLEEP_STYLE[sleepId].star }}星</p>
-                <div v-if="SPO_DATA[gmItem.id] && SPO_DATA[gmItem.id][sleepId] && SPO_DATA[gmItem.id][sleepId].spo">
-                  <p>SPO:{{ SPO_DATA[gmItem.id][sleepId].spo }}</p>
-                  <p>SPOID:{{ SPO_DATA[gmItem.id][sleepId].id }}</p>
-                </div>
-                <div v-else>没有spo</div>
-                <p>{{ SLEEP_STYLE[sleepId].exp }}</p>
-                <p>{{ SLEEP_STYLE[sleepId].shards }}</p>
-                <p>{{ SLEEP_STYLE[sleepId].candys }}</p>
-              </div>
-            </template>
+        </h4>
+        <template v-for="sleepId in levelItem.sleepStyles">
+          <div
+            class="poke-tb__item"
+            v-if="SLEEP_STYLE[sleepId]"
+            v-bind:key="sleepId"
+          >
+            <CptPoke :pokeId="SLEEP_STYLE[sleepId].pokeId" />
+            <p>{{ SLEEP_STYLE[sleepId].id }}</p>
+            <p>{{ SLEEP_NAMES[SLEEP_STYLE[sleepId].sleepNameId] }}</p>
+            <p>{{ SLEEP_STYLE[sleepId].star }}星</p>
+            <div
+              v-if="
+                SPO_DATA[gmItem.id] &&
+                SPO_DATA[gmItem.id][sleepId] &&
+                SPO_DATA[gmItem.id][sleepId].spo
+              "
+            >
+              <p>SPO:{{ SPO_DATA[gmItem.id][sleepId].spo }}</p>
+              <p>SPOID:{{ SPO_DATA[gmItem.id][sleepId].id }}</p>
+            </div>
+            <div v-else>没有spo</div>
+            <p>{{ SLEEP_STYLE[sleepId].exp }}</p>
+            <p>{{ SLEEP_STYLE[sleepId].shards }}</p>
+            <p>{{ SLEEP_STYLE[sleepId].candys }}</p>
           </div>
         </template>
       </div>
-      <div class="poke-tb">
-        <template v-for="pokeItem in pokedex" v-bind:key="pokeItem.id">
-          <template
-            v-for="sleepId in SLEEP_STYLE"
-            v-bind:key="`${sleepId.pokeId}_${sleepId.id}`"
-          >
-            <div class="poke-tb__item" v-if="pokeItem.id === sleepId.pokeId">
-              <CptPoke :pokeId="sleepId.pokeId" />
-              <p>{{ sleepId.id }}</p>
-              <p>{{ SLEEP_NAMES[sleepId.sleepNameId] }}</p>
-              <p>{{ sleepId.star }}星</p>
-              <p>{{ sleepId.exp }}</p>
-              <p>{{ sleepId.shards }}</p>
-              <p>{{ sleepId.candys }}</p>
-            </div>
-          </template>
-        </template>
-      </div>
+    </template>
+  </div>
+  <div class="poke-tb hide">
+    <template v-for="pokeItem in pokedex" v-bind:key="pokeItem.id">
+      <template
+        v-for="sleepItem in SLEEP_STYLE"
+        v-bind:key="`${sleepItem.pokeId}_${sleepItem.id}`"
+      >
+        <div class="poke-tb__item" v-if="pokeItem.id === sleepItem.pokeId">
+          <CptSleepStyle
+            :showMapLevel="true"
+            :sleepItem="sleepItem"
+            :showKey="['sleepType']"
+          />
+        </div>
+      </template>
+    </template>
+  </div>
 </template>
