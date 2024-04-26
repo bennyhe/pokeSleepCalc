7bd3e5818f4980ebf563fa8de64b3ee05f72e63a
bennyhe <hesiyuan009@gmail.com>
2024-04-26 19:18:41 +0800

    feat: 同步最新食材率&技能率

--- src/components/DialogSupport/supportDetail.js
+++ src/components/DialogSupport/supportDetail.js
@@ -24,6 +24,6 @@ const markdownText = `
 # 特别鸣谢
 1. 岛屿只数区间表 by @skysland
 2. [睡姿期望相关](https://nga.178.com/read.php?tid=39443018) by @samuelwyf
-3. 食材率&技能率等数据来源自[Pokémon Sleep 攻略站](https://pks.raenonx.cc/)
+3. 食材率&技能率等数据来源自[Pokémon Sleep 攻略站](https://pks.raenonx.cc/zh/info/production)
 `
 export default markdownText
