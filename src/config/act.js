a29817f132ca3348148da161dd1dd9490cfe3302
bennyhe <hesiyuan009@gmail.com>
2024-07-21 10:58:57 +0800

    feat: add next week act

--- src/config/act.js
+++ src/config/act.js
@@ -1,47 +1,30 @@
-const ACT_LIST = [{
-  name: '夏日嘉年華',
-  namejp: 'サマーフェスティバル',
-  startTime: 1718568000000,
-  endTime: 1719172740000,
-  notArea: [2],
-  midUp: [
-    7,
-    8,
-    9,
-    10,
-    11,
-    12,
-    54,
-    55,
-    79,
-    80,
-    84,
-    85,
-    127,
-    134,
-    158,
-    159,
-    160,
-    199,
-    214,
-    225,
-    333,
-    845
-  ]
-}, {
-  name: '1週年紀念嘉年華',
-  namejp: '1周年記念フェスティバル',
-  startTime: 1720963486934,
-  endTime: 1721591940000,
-  notArea: [1,2,3,4],
-  midUp: [
-    906, 909, 912
-  ]
-}]
-const SLEEP_CALC_CONFIG = {
+export const SLEEP_CALC_CONFIG = {
   onOffBan: false,
   showBan: false,
   banPokes: [],
   showBanArea: []
 }
-export { ACT_LIST, SLEEP_CALC_CONFIG }
+export const ACT_LIST = [{
+  name: '夏日嘉年華',
+  namejp: 'サマーフェスティバル',
+  startTime: 1718568000000,
+  endTime: 1719172740000,
+  notArea: [2],
+  midUp: [7, 8, 9, 10, 11, 12, 54, 55, 79, 80, 84, 85, 127, 134, 158, 159, 160, 199, 214, 225, 333, 845]
+},
+{
+  name: '1週年紀念嘉年華',
+  namejp: '1周年記念フェスティバル',
+  startTime: 1720963486934,
+  endTime: 1721591999000,
+  notArea: [1, 2, 3, 4],
+  midUp: [906, 909, 912]
+},
+{
+  name: '1週年紀念嘉年華',
+  namejp: '1周年記念フェスティバル',
+  startTime: 1721592000000,
+  endTime: 1722196799000,
+  notArea: [3],
+  midUp: [906, 907, 908, 909, 910, 911, 912, 913, 914]
+}]
