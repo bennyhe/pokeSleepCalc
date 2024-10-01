fcde83c993493c28f6a217d2f3f8db87a563b051
bennyhe <hesiyuan009@gmail.com>
2024-10-01 18:42:52 +0800

    feat: init map6

--- src/config/game.js
+++ src/config/game.js
@@ -3,6 +3,7 @@ import map2 from './gameMap/2.js'
 import map3 from './gameMap/3.js'
 import map4 from './gameMap/4.js'
 import map5 from './gameMap/5.js'
+import map6 from './gameMap/6.js'
 
 export const areaBonusMax = 60
 export const mapSplitVer = '2.5.2'
@@ -11,5 +12,6 @@ export const gameMap = [
   map2,
   map3,
   map4,
-  map5
+  map5,
+  map6
 ]
---
+++ src/config/gameMap/6.js
@@ -0,0 +1,261 @@
+import { map6ScoreList } from './scoreLine.js'
+export default {
+  // name: '宝蓝湖畔',
+  id: 'plant',
+  pic: 'map6',
+  berry: [13, 2, 8],
+  scoreList: map6ScoreList,
+  levelList: [
+    {
+      'name': '普通1',
+      'nameIndex': 1,
+      'nameId': 1,
+      'energy': 0,
+      'sleepStyles': [
+      ]
+    },
+    {
+      'name': '普通2',
+      'nameIndex': 2,
+      'nameId': 1,
+      'energy': 12938,
+      'sleepStyles': [
+      ]
+    },
+    {
+      'name': '普通3',
+      'nameIndex': 3,
+      'nameId': 1,
+      'energy': 29756,
+      'sleepStyles': [
+      ]
+    },
+    {
+      'name': '普通4',
+      'nameIndex': 4,
+      'nameId': 1,
+      'energy': 48515,
+      'sleepStyles': [
+      ]
+    },
+    {
+      'name': '普通5',
+      'nameIndex': 5,
+      'nameId': 1,
+      'energy': 71156,
+      'sleepStyles': [
+      ]
+    },
+    {
+      'name': '超级1',
+      'nameIndex': 1,
+      'nameId': 2,
+      'energy': 97031,
+      'sleepStyles': [
+      ]
+    },
+    {
+      'name': '超级2',
+      'nameIndex': 2,
+      'nameId': 2,
+      'energy': 130668,
+      'sleepStyles': []
+    },
+    {
+      'name': '超级3',
+      'nameIndex': 3,
+      'nameId': 2,
+      'energy': 171420,
+      'sleepStyles': []
+    },
+    {
+      'name': '超级4',
+      'nameIndex': 4,
+      'nameId': 2,
+      'energy': 219936,
+      'sleepStyles': []
+    },
+    {
+      'name': '超级5',
+      'nameIndex': 5,
+      'nameId': 2,
+      'energy': 272333,
+      'sleepStyles': []
+    },
+    {
+      'name': '高级1',
+      'nameIndex': 1,
+      'nameId': 3,
+      'energy': 328610,
+      'sleepStyles': []
+    },
+    {
+      'name': '高级2',
+      'nameIndex': 2,
+      'nameId': 3,
+      'energy': 388122,
+      'sleepStyles': []
+    },
+    {
+      'name': '高级3',
+      'nameIndex': 3,
+      'nameId': 3,
+      'energy': 452809,
+      'sleepStyles': []
+    },
+    {
+      'name': '高级4',
+      'nameIndex': 4,
+      'nameId': 3,
+      'energy': 522025,
+      'sleepStyles': []
+    },
+    {
+      'name': '高级5',
+      'nameIndex': 5,
+      'nameId': 3,
+      'energy': 596086,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师1',
+      'nameIndex': 1,
+      'nameId': 4,
+      'energy': 675330,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师2',
+      'nameIndex': 2,
+      'nameId': 4,
+      'energy': 760123,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师3',
+      'nameIndex': 3,
+      'nameId': 4,
+      'energy': 850851,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师4',
+      'nameIndex': 4,
+      'nameId': 4,
+      'energy': 958702,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师5',
+      'nameIndex': 5,
+      'nameId': 4,
+      'energy': 1075709,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师6',
+      'nameIndex': 6,
+      'nameId': 4,
+      'energy': 1202596,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师7',
+      'nameIndex': 7,
+      'nameId': 4,
+      'energy': 1340143,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师8',
+      'nameIndex': 8,
+      'nameId': 4,
+      'energy': 1489190,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师9',
+      'nameIndex': 9,
+      'nameId': 4,
+      'energy': 1650645,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师10',
+      'nameIndex': 10,
+      'nameId': 4,
+      'energy': 1825483,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师11',
+      'nameIndex': 11,
+      'nameId': 4,
+      'energy': 2014758,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师12',
+      'nameIndex': 12,
+      'nameId': 4,
+      'energy': 2219603,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师13',
+      'nameIndex': 13,
+      'nameId': 4,
+      'energy': 2441243,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师14',
+      'nameIndex': 14,
+      'nameId': 4,
+      'energy': 2680993,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师15',
+      'nameIndex': 15,
+      'nameId': 4,
+      'energy': 2925574,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师16',
+      'nameIndex': 16,
+      'nameId': 4,
+      'energy': 3188730,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师17',
+      'nameIndex': 17,
+      'nameId': 4,
+      'energy': 3454577,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师18',
+      'nameIndex': 18,
+      'nameId': 4,
+      'energy': 3739033,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师19',
+      'nameIndex': 19,
+      'nameId': 4,
+      'energy': 4166848,
+      'sleepStyles': []
+    },
+    {
+      'name': '大师20',
+      'nameIndex': 20,
+      'nameId': 4,
+      'energy': 5193272,
+      'sleepStyles': []
+    }
+  ]
+}
