5de1743fc2fccdb5ff1ce31f2d984a69a3fecf0b
bennyhe <hesiyuan009@gmail.com>
2025-03-28 22:36:32 +0800

    feat: 1.2倍活动睡意

--- src/pages/SleepCalc.vue
+++ src/pages/SleepCalc.vue
@@ -947,6 +947,9 @@ const getQuickChangeSleepPoint = () => {
       <el-form-item :label="$t('PAGE_SLEEPCALC.formLableDays')">
         <el-radio-group v-model="userData.times" @change="handleClickTimes()">
           <el-radio :label="1">{{ $t("OPTIONS.otherDay") }}</el-radio>
+          <el-radio :label="1.2"
+            ><CptAvatar pokeId="488" />*1.2倍</el-radio
+          >
           <el-radio :label="1.5"
             ><SvgIcon type="moonNight" size="mid" />{{
               $t("OPTIONS.sleepDay")
